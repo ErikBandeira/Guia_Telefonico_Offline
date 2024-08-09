61dd7d459657bf93bf14f7c09e8533c832b60db6
ErikBandeira <141233611+ErikBandeira@users.noreply.github.com>
2024-08-09 11:26:26 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Guia Telefônico Offline',
       theme: ThemeData(
         primaryColor: const Color(0xFF41DBDB),
         appBarTheme: const AppBarTheme(
@@ -40,44 +40,130 @@ class _MyHomePageState extends State<MyHomePage> {
     'Sul',
   ];
 
+  void _navigateToDetailsPage(String region) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailsPage(region: region),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFF207676),
-        appBar: AppBar(
-          title: const Text(
-            'Lista Telefônica',
-            style: TextStyle(
-              fontSize: 33,
+      backgroundColor: const Color(0xFF207676),
+      appBar: AppBar(
+        title: const Text(
+          'Lista Telefônica',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          height: 695, // Defina a altura máxima da lista
+          width: 550, // Defina a largura máxima da lista
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(
+                255, 44, 163, 163), // Cor do fundo da caixa
+            border: Border.all(
+              color: const Color.fromARGB(255, 44, 163, 163),
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(35.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Padding(
+                    padding:
+                        EdgeInsets.all(30.0), // Espaçamento ao redor do título
+                    child: Text(
+                      'Regiões',
+                      style: TextStyle(
+                        fontSize: 50, // Tamanho da fonte do título
+                        color: Colors.white, // Cor da fonte do título
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: entries.map((entry) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical:
+                                9.0), // Espaçamento vertical entre os botões
+                        child: SizedBox(
+                          width: 450, // Largura fixa dos botões
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(
+                                  0xFF207676), // Cor de fundo do botão
+                              padding: const EdgeInsets.all(
+                                  20.0), // Espaçamento interno
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    40.0), // Bordas arredondadas
+                              ),
+                            ),
+                            onPressed: () => _navigateToDetailsPage(entry),
+                            child: Text(
+                              entry,
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 125, 245, 245),
+                                fontSize:
+                                    40, // Ajuste o tamanho da fonte conforme necessário
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 289,
-                width: 200,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: entries.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: SizedBox(
-                          width: 300,
-                          child: Text(
-                            entries[index],
-                            style: const TextStyle(
-                                color: Colors.white, fontSize: 27),
-                            maxLines: 1,
-                          )),
-                    );
-                  },
-                ),
-              ),
-            ],
+      ),
+    );
+  }
+}
+
+//PAGINAS DOS TELEFONES DAS REGIÕES
+
+class DetailsPage extends StatelessWidget {
+  final String region;
+
+  const DetailsPage({super.key, required this.region});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF207676),
+      appBar: AppBar(
+        title: Text(region),
+        centerTitle: true, // Isso vai centralizar o título
+        titleTextStyle: const TextStyle(
+          fontSize: 45,
+          color: Colors.white, // Define a cor do texto do título
+        ),
+      ),
+      body: Center(
+        child: Container(
+          height: 695, // Defina a altura máxima da lista
+          width: 550, // Defina a largura máxima da lista
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(
+                255, 44, 163, 163), // Cor do fundo da caixa
+            border: Border.all(
+              color: const Color.fromARGB(255, 44, 163, 163),
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
